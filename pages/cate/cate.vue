c9635a2bf239c88847ec610c5b7613bffe221530
GuanSzzz <847388139@qq.com>
2022-09-05 11:42:12 +0800

    完成了布局

---
+++ pages/cate/cate.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    fenlei
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
